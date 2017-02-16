e6348af15a2b22ce68af4ca9071e059f3b99d302
EstebanRH23 <rojasesteban23@gmail.com>
2017-02-16 13:42:17 -0600

    Create 2163.js

---
+++ 2163.js
@@ -0,0 +1,14 @@
+ <!doctype html>
+ <html lang="es">
+ <head>
+ <meta charset="utf-8">
+ <title> Ejemplo con guion externo </title>
+ </head>
+ <body>
+ <hl>Ejemplo con guion externo</hl>
+<canvas id="miCanvas" width="100" height="100">
+&iexcl;No funciona!
+</canvas>
+<script src="2163.js"></script>
+</body>
+</html>
